3386f1dad41d945045248cdeb2a82402259e76af
Samuel Guyah <septimusguy@gmail.com>
2024-08-19 11:26:45 +0200

    new theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,12 +5,14 @@ return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- black and white theme
   { "seandewar/paragon.vim", name = "paragon", priority = 1000 },
+  -- black and white but high contrast
+  { "plan9-for-vimspace/acme-colors", name = "acme", priority = 1000 },
 
   -- Configure LazyVim to load the desired plugin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "paragon",
+      colorscheme = "acme",
     },
   },
 }
