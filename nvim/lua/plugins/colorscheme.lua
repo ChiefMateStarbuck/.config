b387b4fc6c69b1863457e16693918b806b14410c
Samuel Guyah <septimusguy@gmail.com>
2024-09-18 11:50:42 +0200

    got somewhere

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,54 @@
+local black = "#000000"
 return {
-  -- load all themes
-
-  -- light color theme
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  -- black and white theme
-  { "seandewar/paragon.vim", name = "paragon", priority = 1000 },
-  -- black and white but high contrast
-  { "plan9-for-vimspace/acme-colors", name = "acme", priority = 1000 },
-  -- maybe better black and white?
-  { "gaelph/nano.nvim" },
-  { "patagia/dieter.nvim" },
-  { "Verf/deepwhite.nvim" },
-  -- Configure LazyVim to load the desired plugin
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "",
-    },
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        palettes = {
+          github_light = {
+            comment = black,
+            fg0 = black,
+            fg1 = black,
+            fg2 = black,
+            fg3 = black,
+          },
+        },
+        specs = {
+          github_light = {
+            syntax = {
+              comment = black,
+              constant = black,
+              string = black,
+              character = black,
+              number = black,
+              float = black,
+              boolean = black,
+              identifier = black,
+              func = black,
+              statement = black,
+              conditional = black,
+              label = black,
+              operator = black,
+              keyword = black,
+              exception = black,
+              preproc = black,
+              include = black,
+              define = black,
+              macro = black,
+              PreCondit = black,
+              type = black,
+              storageclass = black,
+              structure = black,
+              typedef = black,
+              special = black,
+            },
+          },
+        },
+      })
+
+      vim.cmd("colorscheme github_light")
+    end,
   },
 }
